277467bf9b7d861bd8c412571621b034395cc62b
shivamrai <shivam.rai.111@gmail.com>
2019-11-11 16:53:43 -0800

    adding 2 linked list complete

--- Adding2LinkedLists.py
+++ Adding2LinkedLists.py
@@ -1,35 +1,43 @@
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.next = None
+Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
 
 class Solution:
-    def __init__(self):
-        self.head = None
-        
-    def reverse(self, l1: ListNode): 
-        prev = None
-        current = self.head 
-        while(current is not None): 
-            next = current.next
-            current.next = prev 
-            prev = current 
-            current = next
-        self.head = prev 
         
     def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
-        l3 = ListNode()
         carry = 0
-        sumofl1Andl2CurrentNode = 0
-        numberString = 
-        while l1 & l2:
-            sumofl1Andl2CurrentNode = l1.val + l2.val + carry
-            carry = sumofl1Andl2CurrentNode/10
-            l3.val = sumofl1Andl2CurrentNode%10
-            l1 = l1.next
-            l2 = l2.next
-        self.reverse(l3)
-        while l3:
+        link_next = None
+        linkedListSum = None
+        first = None
+        while l1 or l2 or carry:
+            # calculcate sum of digits
+            value = 0
+            if l1:
+                value += l1.val
+            if l2:
+                value += l2.val
+            value += carry
+            if value > 9:
+                carry = 1
+                value -= 10
+            else:
+                carry = 0
+            
+            # add new node
+            if first:
+                linkedListSum.next = ListNode(value)
+                linkedListSum = linkedListSum.next
+            else:
+                linkedListSum = ListNode(value)
+                first = linkedListSum
+            
+            # move to next digits
+            if l1:
+                l1 = l1.next
+            if l2:
+                l2 = l2.next
+        return first
             
             
