066773cb73819e358235743d3d3d852e2117bc5e
Shivam Rai <shivam.rai.111@gmail.com>
2019-11-17 17:13:12 -0800

    more practice

---
+++ stringtonumberAtoi.py
@@ -0,0 +1,27 @@
+def myAtoi(str):
+    sign = 1
+    num = 0
+    if (str.strip == ""):
+        return 0
+    if (str.strip()[0]=="-"):
+        sign = -1
+    str=str.strip()
+    for x,y in enumerate(str):
+        if((y=='+' or y=='-') and x==0):
+            pass
+        elif(y.isdigit()):
+            num = num * 10 + int(y)
+        else:
+            break
+    num=num*sign
+    if(num>(2**31)-1):
+        return (2**31)-1
+    elif(num<-2**31):
+        return (-2**31)
+    else:
+        return num
+
+if __name__ == "__main__":
+    str="     -49 is here"
+    print(str.strip())
+    print(myAtoi(str))
