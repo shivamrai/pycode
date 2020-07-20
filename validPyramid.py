e6a1f86c089577f5d39cff2d408ae1360cc4a4ba
Shivam Rai <shivam.rai.111@gmail.com>
2020-07-20 16:34:41 -0700

    validPyramid solution

---
+++ validPyramid.py
@@ -0,0 +1,29 @@
+def validPyramid(A: list) -> bool:
+    if(A==[] or len(A)<3):
+        return False
+    getMax=max(A)
+    print(getMax)
+    i=0
+    j=A.index(getMax)
+    validPyramidBool = False
+    if(j==len(A)-1 or j==0):
+        return False
+    else:
+        while(i<j):
+            if(A[i]<A[i+1]):
+                validPyramidBool = True
+            else:
+                validPyramidBool = False
+                return validPyramidBool
+            i+=1
+        while(j<len(A)-1):
+            if(A[j]>A[j+1]):
+                validPyramidBool = True
+            else:
+                validPyramidBool=False
+                break
+            j+=1
+    return validPyramidBool
+if __name__ == "__main__":
+    listA=[2,1]
+    print(validPyramid(listA))
