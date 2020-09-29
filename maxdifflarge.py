0f4b1f230f416971e57a96b5ce3f89f95bbfb1a4
Shivam Rai <shivam.rai.111@gmail.com>
2020-09-29 10:46:55 -0700

    4 problems with solutions

---
+++ maxdifflarge.py
@@ -0,0 +1,21 @@
+def maxDiff(arr, n): 
+    diff = [0] * (n - 1) 
+    for i in range (0, n-1): 
+        diff[i] = arr[i+1] - arr[i] 
+          
+    # Now find the maximum sum  
+    # subarray in diff array     
+    max_diff = diff[0] 
+    for i in range(1, n-1): 
+        if (diff[i-1] > 0): 
+            diff[i] += diff[i-1] 
+      
+        if (max_diff < diff[i]): 
+            max_diff = diff[i] 
+      
+    return max_diff 
+
+if __name__ == "__main__":
+    arr = [2,3,10,6,4,8,1]
+    arr2 = [6,3,1,2,27,5,7,8,2]
+    print(maxDiff(arr2,len(arr2)))
---
+++ throttling.py
@@ -0,0 +1,23 @@
+def throttlinggateway(requesttime):
+    reqcounter = 0
+    timeset = 0
+    dropped = 0
+    reqdict = {}
+    for element in requesttime:
+        if(element not in reqdict and element - timeset <9 and reqcounter<20):
+            reqdict[element] = 1
+            reqcounter+=1
+        elif(element in reqdict and reqdict[element]<3 and element - timeset <9 and reqcounter<20):
+            reqdict[element] += 1
+            reqcounter+=1
+            if(element - timeset>10):
+                timeset+=10
+        else:
+            dropped+=1
+            if(reqcounter>20 and timeset>60):
+                reqcounter = 0
+    return dropped
+
+if __name__ == "__main__":
+    requesttime = [1,1,1,1,2,2,3,3,3,4,4,4,5,5,5,6,6,6,7,7,7,7,11,11,11,11]
+    print(throttlinggateway(requesttime))
