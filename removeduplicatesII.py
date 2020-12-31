ab6411199cc62586769f1801c18acda2fbe94865
shivamrai <shivam.rai.111@gmail.com>
2020-12-30 19:11:34 -0800

    windows questions

---
+++ removeduplicatesII.py
@@ -0,0 +1,18 @@
+class Solution:
+    def removeDuplicates(self, nums: list) -> int:
+        dup = 0
+        size = len(nums)
+        i=1
+        while(i<size):
+            if(dup==2):
+                nums.pop(i)
+            if(nums[i-1]==nums[i]):
+                dup+=1
+            else:
+                dup=0
+            size = len(nums)
+            i+=1
+        return len(nums)
+if __name__ == "__main__":
+    x = Solution()
+    print(x.removeDuplicates([1,1,1,2,2,3]))
