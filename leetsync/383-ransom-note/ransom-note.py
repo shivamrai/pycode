652866559df2d59ddc5f25009690b78c9362cbc3
Shivam Rai <shivam.rai.111@gmail.com>
2026-03-02 15:29:28 -0500

    Time: 29 ms (15.92%) | Memory: 19.5 MB (87.37%) - LeetSync

--- leetsync/383-ransom-note/ransom-note.py
+++ leetsync/383-ransom-note/ransom-note.py
@@ -1,5 +1,7 @@
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        if len(ransomNote)>len(magazine):
+            return False
         mag_dict = {}
         for x in magazine:
             if x in mag_dict:
