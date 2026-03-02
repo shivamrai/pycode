9d99a213ce1da42199c1c10254edbfa595b9e977
Shivam Rai <shivam.rai.111@gmail.com>
2026-03-02 15:12:22 -0500

    Time: 0 ms (100.00%) | Memory: 19.5 MB (25.31%) - LeetSync

--- leetsync/1444-number-of-steps-to-reduce-a-number-to-zero/number-of-steps-to-reduce-a-number-to-zero.py
+++ leetsync/1444-number-of-steps-to-reduce-a-number-to-zero/number-of-steps-to-reduce-a-number-to-zero.py
@@ -3,7 +3,7 @@ class Solution:
         steps=0
         while num != 0:
             if num%2==0:
-                num/=2
+                num//=2
                 steps+=1
             elif num==0:
                 break
