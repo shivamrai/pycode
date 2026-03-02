27a127da60b267ee27f5ffdfd9b27a9f4d470125
Shivam Rai <shivam.rai.111@gmail.com>
2026-03-02 15:28:16 -0500

    Time: 31 ms (12.86%) | Memory: 19.7 MB (68.95%) - LeetSync

---
+++ leetsync/383-ransom-note/ransom-note.py
@@ -0,0 +1,14 @@
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        mag_dict = {}
+        for x in magazine:
+            if x in mag_dict:
+                mag_dict[x] += 1
+            else:
+                mag_dict[x] = 1
+        for x in ransomNote:
+            if x in mag_dict and mag_dict[x] > 0:
+                mag_dict[x] -= 1
+            else:
+                return False
+        return True
