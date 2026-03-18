95392164edde60beee93d0ff428bc83113ce3e9b
Shivam Rai <shivam.rai.111@gmail.com>
2026-03-18 00:12:49 -0400

    Time: 143 ms (39.46%) | Memory: 68.8 MB (86.36%) - LeetSync

---
+++ leetsync/1023-time-based-key-value-store/time-based-key-value-store.py
@@ -0,0 +1,34 @@
+from collections import defaultdict
+class TimeMap:
+
+    def __init__(self):
+        self.time_map = defaultdict(list)
+
+    def __binary_key_search(self, currtimestamp: int, timestamps: list):
+        low = 0
+        high = len(timestamps) - 1
+        result = ""
+        while low <= high:
+            mid = (low + high) // 2
+            if timestamps[mid][0] <= currtimestamp:
+                result = timestamps[mid][1]  # Valid candidate
+                low = mid + 1             # Search right for larger ts
+            else:
+                high = mid - 1            # Search left
+        return result
+
+    def set(self, key: str, value: str, timestamp: int) -> None:
+        self.time_map[key].append((timestamp, value))
+
+    def get(self, key: str, timestamp: int) -> str:
+        if key in self.time_map:
+            values = self.time_map[key]  # Get the [(ts, val), ...] list
+            return self.__binary_key_search(timestamp, values)
+        else:
+            return ""
+
+
+# Your TimeMap object will be instantiated and called as such:
+# obj = TimeMap()
+# obj.set(key,value,timestamp)
+# param_2 = obj.get(key,timestamp)
