660be1c3515395f0d5138d5792a3a9063c055e7e
Shivam Rai <shivam.rai.111@gmail.com>
2026-03-20 22:26:59 -0400

    Time: 0 ms (100.00%) | Memory: 19.2 MB (91.63%) - LeetSync

--- leetsync/747-min-cost-climbing-stairs/min-cost-climbing-stairs.py
+++ leetsync/747-min-cost-climbing-stairs/min-cost-climbing-stairs.py
@@ -1,16 +1,11 @@
 class Solution:
     def minCostClimbingStairs(self, cost: List[int]) -> int:
-        # step_selection = cost[0] if cost[0] < cost[1] else cost[1]
         length = len(cost)
-        # prev, curr = 0 , 0
         dp = [0] * length
         dp[-1], dp[-2] = cost[-1], cost[-2]
         i = length - 3
         while i >= 0:
-            #print(f"{cost[i]} {dp[i+1]} {dp[i+2]}")
             dp[i] = min(dp[i+1], dp[i+2]) + cost[i]
             i-=1
 
-        for _, x in enumerate(dp):
-            print(x)
         return min(dp[0], dp[1])
