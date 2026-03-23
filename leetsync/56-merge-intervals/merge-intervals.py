ba05bbd4cf3a3b18ad49b0d66096d888e6cc7c31
Shivam Rai <shivam.rai.111@gmail.com>
2026-03-22 22:57:36 -0400

    Time: 7 ms (71.30%) | Memory: 22.3 MB (93.33%) - LeetSync

--- leetsync/56-merge-intervals/merge-intervals.py
+++ leetsync/56-merge-intervals/merge-intervals.py
@@ -1,14 +1,25 @@
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
-        i = 1
+        # i = 1
+        # intervals.sort(key = lambda x: x[0])
+        # while i < len(intervals):
+        #     if intervals[i-1][1] >= intervals[i][0]:
+        #         start = intervals[i-1][0]
+        #         end = max(intervals[i-1][1], intervals[i][1])
+        #         del intervals[i-1]
+        #         del intervals[i-1]
+        #         intervals.insert(i-1, [start, end])
+        #     else:
+        #         i+=1
+        # return intervals
         intervals.sort(key = lambda x: x[0])
-        while i < len(intervals):
-            if intervals[i-1][1] >= intervals[i][0]:
-                start = intervals[i-1][0]
-                end = max(intervals[i-1][1], intervals[i][1])
-                del intervals[i-1]
-                del intervals[i-1]
-                intervals.insert(i-1, [start, end])
+        res = [intervals[0]]  # Initialize result with first interval
+        for i in range(1, len(intervals)):
+            # Check if current interval overlaps with last merged interval
+            if res[-1][1] >= intervals[i][0]:
+                # Merge by updating end to maximum of both ends
+                res[-1][1] = max(res[-1][1], intervals[i][1])
             else:
-                i+=1
-        return intervals
+                # No overlap, append current interval to result
+                res.append(intervals[i])
+        return res
