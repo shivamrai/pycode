5a68d99f3a7a344bf96903a72f51094a2a57b3fd
Shivam Rai <shivam.rai.111@gmail.com>
2026-03-26 17:05:27 -0400

    Time: 1 ms (92.43%) | Memory: 20.2 MB (94.18%) - LeetSync

---
+++ leetsync/547-number-of-provinces/number-of-provinces.py
@@ -0,0 +1,19 @@
+class Solution:
+    def _dfs(self, isConnected, i, visited):
+        for neighbor, connected in enumerate(isConnected[i]):
+            if connected == 1 and neighbor not in visited:
+                visited.add(neighbor)
+                self._dfs(isConnected ,neighbor, visited)
+        
+    def findCircleNum(self, isConnected: List[List[int]]) -> int:
+        n = len(isConnected)
+        visited = set()
+        count = 0
+        
+        for i in range(n):
+            if i not in visited:
+                count += 1
+                visited.add(i) # Mark the "start" of the province
+                self._dfs(isConnected, i, visited)
+                
+        return count
