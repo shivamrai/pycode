422bd68d098edd06fab98be29c335eaf3e2c0257
Shivam Rai <shivam.rai.111@gmail.com>
2026-04-19 23:09:07 -0400

    Time: 17 ms (26.42%) | Memory: 23.9 MB (29.21%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -1,15 +1,26 @@
-from collections import defaultdict, Counter
-
+from collections import defaultdict
 class Solution:
 
+    # def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+    #     word_ctr = defaultdict(list)
+    #     res = []
+    #     for word in strs:
+    #         key = tuple(sorted(Counter(word).items()))
+    #         word_ctr[key].append(word)
+                
+    #     for v in word_ctr.values():
+    #         res.append(v)
+        
+    #     return res
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         word_ctr = defaultdict(list)
-        res = []
         for word in strs:
-            key = tuple(sorted(Counter(word).items()))
+            # Create a count array for 26 lowercase letters
+            count = [0] * 26
+            for c in word:
+                count[ord(c) - ord('a')] += 1
+            # Use tuple of counts as key (immutable and hashable)
+            key = tuple(count)
             word_ctr[key].append(word)
-                
-        for v in word_ctr.values():
-            res.append(v)
-        
-        return res
+        # Return all groups directly
+        return list(word_ctr.values())
